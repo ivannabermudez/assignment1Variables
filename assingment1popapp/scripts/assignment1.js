75eeea8c09689a2496ce6a4559dc13c9c6c4ed0b
ivannabermudez <112995668+ivannabermudez@users.noreply.github.com>
2023-02-01 20:13:11 -0800

    Add files via upload

---
+++ assingment1popapp/scripts/assignment1.js
@@ -0,0 +1,30 @@
+// Assignment 1
+// Programming Fundamentals
+// Think in a real and popular web application such as Netflix, Amazon, Youtube, etc. abstract that real world, create an script and declare 20 different variables, assign values to such variables and display them on the HTML, using template string format.
+
+// ** use document.write(``);
+
+console.log("PopularAppScript");
+var login = login = true;
+var username = "username";
+var password = "password"; 
+var email = "email"; 
+var forgotPassword = "forgot password"; 
+var payment = "payment type";
+var download = true; //boolean
+var like = true; //boolean
+var subscribe = true; //boolean
+var addToFavorites = "add to favorites"; 
+var changeUser = true; //boolean
+var Logout = true; //boolean
+var EditIcon = "edit icon";
+var subtitles = true; 
+var audio = true; 
+var version = Number; 
+var account2 = "switch account";
+var kidsAccount = true; 
+var Exit = true; 
+var Logout = login = false; 
+
+document.write('<p class="text"> Login:' + login + '</p><p> Enter username:' + username +'</p><p> Enter password:' +password + '</p><p> Enter email:' + email + '</p><p> Forgot your password?:' + forgotPassword + '</p><p> Enter Payment:' + payment + '</p>')
+
